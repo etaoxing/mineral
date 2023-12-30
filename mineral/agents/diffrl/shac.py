de4da443ebf5436ad2276fed0390bb6414f8e07f
etaoxing <etaoxing@users.noreply.github.com>
2023-12-30 18:37:38 -0500

    Refactor -> `SHAC.get_actions(...)` - remove `clamp_actions(...)`

--- mineral/agents/diffrl/shac.py
+++ mineral/agents/diffrl/shac.py
@@ -151,8 +151,11 @@ class SHAC(ActorCriticBase):
         self.mus = torch.zeros((T, B, self.num_actions), dtype=torch.float32, device=self.device)
         self.sigmas = torch.zeros((T, B, self.num_actions), dtype=torch.float32, device=self.device)
 
-    def clamp_actions(self, actions):
-        return torch.tanh(actions)
+    def get_actions(self, obs, sample: bool = True):
+        actions = self.actor(obs['obs'], deterministic=not sample)
+        # clamp actions
+        actions = torch.tanh(actions)
+        return actions
 
     def compute_actor_loss(self, deterministic=False):
         rew_acc = torch.zeros((self.horizon_len + 1, self.num_envs), dtype=torch.float32, device=self.device)
@@ -187,8 +190,7 @@ class SHAC(ActorCriticBase):
                     self.obs_buf[k][i] = v.clone()
 
             # take env step
-            actions = self.actor(obs['obs'], deterministic=deterministic)
-            actions = self.clamp_actions(actions)
+            actions = self.get_actions(obs, sample=not deterministic)
             obs, rew, done, extra_info = self.env.step(actions)
             obs = self._convert_obs(obs)
             self.episode_length += 1
@@ -324,9 +326,7 @@ class SHAC(ActorCriticBase):
             if self.obs_rms is not None:
                 obs = {k: self.obs_rms[k].normalize(v) for k, v in obs.items()}
 
-            actions = self.actor(obs['obs'], deterministic=deterministic)
-            actions = self.clamp_actions(actions)
-
+            actions = self.get_actions(obs, sample=not deterministic)
             obs, rew, done, _ = self.env.step(actions)
             obs = self._convert_obs(obs)
 
